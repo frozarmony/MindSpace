47d56b1a1d1911bdb40e75eb3fb23261763bd6d5
frozarmony <frozarmony@gmail.com>
2014-05-05 09:20:52 +0200

    First Commit

---
+++ MindSpace/MindSpace/src/com/utcLABS/mindspace/model/ConceptModel.java
@@ -0,0 +1,101 @@
+package com.utcLABS.mindspace.model;
+
+import java.util.LinkedList;
+
+import android.graphics.Color;
+
+public class ConceptModel {
+	
+	enum MindSpaceShapes{
+		square,
+		circle,
+		triangle
+	}
+
+	// Data Members
+	private String						name;
+	
+	// Form Members
+	private float						x;
+	private float						y;
+	private Color						color;
+	private MindSpaceShapes				shape;
+	
+	// Link Members
+	private ConceptModel				parent;
+	//private boolean						linked;
+	private LinkedList<ConceptModel>	children;
+	//private LinkedList<ConceptModel>	externalLink;
+	
+	// Constructor
+	public ConceptModel(float x, float y, ConceptModel parent) {
+		super();
+		
+		// Data
+		this.name = "";
+		
+		// Forms
+		this.x = x;
+		this.y = y;
+		this.color = null;
+		this.shape = null;
+
+		// Link
+		if( parent != null )
+			parent.addChildNode(this);
+		else
+			this.parent = null;
+		children = new LinkedList<ConceptModel>();
+	}
+	
+	// Getters & Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public MindSpaceShapes getShape() {
+		return shape;
+	}
+	public void setShape(MindSpaceShapes shape) {
+		this.shape = shape;
+	}
+	public ConceptModel getParent() {
+		return parent;
+	}
+	public void setParent(ConceptModel parent) {
+		this.parent = parent;
+	}
+	public LinkedList<ConceptModel> getChildren(){
+		return children;
+	}
+	
+	// Method
+	public void addChildNode(ConceptModel child){
+		if( child != null && !children.contains(child) ){
+			children.add(child);
+			child.parent = this;
+		}
+	}
+	
+	
+}
---
+++ MindSpace/MindSpace/src/com/utcLABS/mindspace/view/ConceptView.java
@@ -0,0 +1,96 @@
+package com.utcLABS.mindspace.view;
+
+import java.util.LinkedList;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+
+import com.utcLABS.mindspace.model.ConceptModel;
+
+public class ConceptView {
+
+	// Model Member
+	private ConceptModel				model;
+	
+	// View Member
+	private Paint						paint;
+	private Path						parentBranch;
+	private LinkedList<ConceptView>		childrenViews;
+
+	// Constructor
+	public ConceptView(ConceptModel model, float width, float height) {
+		super();
+		this.model = model;
+		
+		// Paint Config
+		this.paint = new Paint();
+    	paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    	paint.setColor(Color.BLACK);
+    	paint.setAntiAlias(true);
+    	paint.setTextAlign(Paint.Align.CENTER);
+    	paint.setTextSize(30);
+    	
+    	// Path Config
+    	ConceptModel parent = model.getParent();
+    	if( parent != null ){
+    		this.parentBranch = new Path();
+    		if(parent.getX()<model.getX()){
+        		this.parentBranch.moveTo(parent.getX()*width, parent.getY()*height);
+        		this.parentBranch.lineTo(model.getX()*width, model.getY()*height);
+    		}
+    		else{
+        		this.parentBranch.moveTo(model.getX()*width, model.getY()*height);
+        		this.parentBranch.lineTo(parent.getX()*width, parent.getY()*height);
+    		}
+    	}
+    	else{
+    		this.parentBranch = null;
+    	}
+    	
+    	// Children Views
+    	this.childrenViews = new LinkedList<ConceptView>();
+    	for( ConceptModel m : model.getChildren() )
+    		this.childrenViews.add(new ConceptView(m, width, height));
+    	
+	}
+	
+	// Draw
+	public void draw(Canvas pCanvas, float width, float height){
+		
+		// Children first
+		for( ConceptView v : this.childrenViews )
+			v.draw(pCanvas,width,height);
+		
+		// This Draw
+		if( this.parentBranch != null ){
+
+			this.parentBranch.reset();
+	    	ConceptModel parent = model.getParent();
+			if(parent.getX()<model.getX()){
+        		this.parentBranch.moveTo(parent.getX()*width, parent.getY()*height);
+        		this.parentBranch.lineTo(model.getX()*width, model.getY()*height);
+    		}
+    		else{
+        		this.parentBranch.moveTo(model.getX()*width, model.getY()*height);
+        		this.parentBranch.lineTo(parent.getX()*width, parent.getY()*height);
+    		}
+			
+			this.paint.setStyle(Paint.Style.STROKE);
+			pCanvas.drawPath(this.parentBranch, this.paint);
+			this.paint.setStyle(Paint.Style.FILL_AND_STROKE);
+			pCanvas.drawTextOnPath(model.getName(), this.parentBranch, 0, 0, this.paint);
+		}
+		else{
+			RectF rect = new RectF(model.getX()*width-50f,model.getY()*height-50f,model.getX()*width+50f,model.getY()*height+50f);
+			pCanvas.drawOval(rect, paint);
+			this.paint.setColor(Color.WHITE);
+			pCanvas.drawText(model.getName(), model.getX()*width, model.getY()*height+10f, paint);
+			this.paint.setColor(Color.BLACK);
+		}
+		
+	}
+	
+}
